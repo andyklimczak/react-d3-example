8a25d88cb3cd8b71ab3c495135319fb43e8f881e
Andy Klimczak <andyklimczak@fastmail.com>
2016-05-15 18:09:19 -0400

    pre a row of buttons

---
+++ src/components/H1BGraph/Controls/ControlRow.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import Toggle from './Toggle';
+
+class ControlRow extends Component {
+  render() {
+    return (
+      <div className='row'>
+        <div className='col-md-12'>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ControlRow;
